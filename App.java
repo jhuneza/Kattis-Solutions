11ad42d6c4ad44b50af41e219b3441bb2d39326d
Llara Bermudez <47492557+jhuneza@users.noreply.github.com>
2023-06-11 19:34:24 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		//input 2 ints
+		int num1 = scnr.nextInt();
+		int num2 = scnr.nextInt();
+		
+		//output 1 if num1 > num2
+		if (num1 > num2) {
+			System.out.println("1");
+		}
+		
+		//else 0
+		else {
+			System.out.println("0");
+		}
+
+	}
+
+}
