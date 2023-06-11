d1228f39edbcdf67a98cb3e76db15b6a75a7fb38
Llara Bermudez <47492557+jhuneza@users.noreply.github.com>
2023-06-11 19:32:01 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		int num1 = scnr.nextInt();
+		//int num2 = scnr.nextInt();
+		int reversedNum = 0;
+		
+		while (num1 != 0) {
+			reversedNum = reversedNum * 10 + num1 % 10;
+			num1 = num1/10;
+		}
+		
+		System.out.println(reversedNum);
+//		
+//		if(num2 == reversedNum) {
+//			System.out.println("yes");
+//		}
+/*		else {
+			System.out.println("no");
+		}
+*/
+	}
+}
